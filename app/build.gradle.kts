07fa414d39e2bc5e99c55d83e7b86cfaf28e114f
M1ster.ToxiC <krivolapovnikita9@gmail.com>
2026-02-26 13:00:11 +0700

    save project

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -26,23 +26,27 @@ android {
             )
         }
     }
+
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
         targetCompatibility = JavaVersion.VERSION_11
     }
+
     kotlinOptions {
         jvmTarget = "11"
     }
 }
 
 dependencies {
-
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
     implementation(libs.androidx.activity)
     implementation(libs.androidx.constraintlayout)
     implementation(libs.androidx.gridlayout)
+
+    implementation("org.zeromq:jeromq:0.5.3")
+
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
